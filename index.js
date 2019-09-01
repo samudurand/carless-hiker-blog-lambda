25de7a27d36e08ce4a2dbc052da7c745c6af7814
Samuel Durand <sam.durand@coopdigital.co.uk>
2019-09-01 18:37:49 +0100

    Establish list of images and their url

--- index.js
+++ index.js
@@ -1,18 +1,81 @@
+const convert = require('xml-js');
+const fs = require('fs');
+const util = require('util');
+const request = require("request-promise");
+const aws = require("aws-sdk");
 
+const readdir = util.promisify(fs.readdir);
+
+// Used to browse the github api trees and list the images
+const postsApiRepoGithubURL = 'https://api.github.com/repos/samudurand/carless-hiker-blog/git/trees/cf1db899423cdbff064210ecd8f9d0564a4d2ff1';
+// Used to download the images
+const postsDirectRepoGithubURL = 'https://github.com/samudurand/carless-hiker-blog/raw/master/source/_posts/';
+
+function extractGithubName(url) {
+    const parts = url.split('/');
+    return `${parts[3]}-${parts[4]}-${parts[5]}-${parts[6]}`;
+}
+
+const getImageUrls = async (postGithubRepoName) => {
+
+    let fileTree = null;
+    try {
+        fileTree = await request({
+            uri: postsApiRepoGithubURL,
+            method: "GET",
+            headers: {
+                'User-Agent': 'samudurand'
+            },
+            json: true
+        });
+    } catch (error) {
+        console.error('Could not retrieve the file tree from github.', error);
+        throw error;
+    }
+
+    const folderUrl = fileTree.tree.find(object => object.path === postGithubRepoName).url;
+
+    let folderTree = null;
+    try {
+        folderTree = await request({
+            uri: folderUrl,
+            method: "GET",
+            headers: {
+                'User-Agent': 'samudurand'
+            },
+            json: true
+        });
+    } catch (error) {
+        console.error('Could not get blog post folder content from github.', error);
+        throw error;
+    }
+
+    return folderTree.tree
+        .map(object => object.path)
+        .filter((name => name.endsWith('jpeg') || name.endsWith('jpg') || name.endsWith('png')))
+        .map(name => postsDirectRepoGithubURL + postGithubRepoName + '/' + name);
+};
 
 exports.handler = async (event, context) => {
 
-    console.log(JSON.stringify(event, null, 2));
+    const rssUpdateContent = JSON.parse(convert.xml2json(event.body, {compact: true, spaces: 2}));
+    const linkToPost = rssUpdateContent.feed.entry.link._attributes.href;
+    const postGithubRepoName = extractGithubName(linkToPost);
+    const imageUrls = await getImageUrls(postGithubRepoName);
 
-    var response = {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/plain"
-        },
-        // "body": JSON.stringify(responseBody),
-        "body": "All good!",
-        "isBase64Encoded": false
+    const result = {
+        "linkToPost": linkToPost,
+        "imageUrls": imageUrls
     };
 
-    return response;
+    console.log(result);
+
+    return  {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": JSON.stringify(result),
+        "isBase64Encoded": false
+    };
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,16 @@
 const xmlBodyJson = require('./resources/test-rss-message.json');
+const rewire = require('rewire');
 const index = require('../index');
 
-test('parse body in rss update message', () => {
-    expect(emptyBodyJson.body).toBe('empty body');
+const indexInternal = rewire('../index.js');
+const extractGithubName = indexInternal.__get__('extractGithubName');
+
+test('extract github name', () => {
+    const name = extractGithubName("http://the-carless-hiker.com/2019/08/18/darwen-tower-to-sunnyhurst-brook/")
+    expect(name).toBe('2019-08-18-darwen-tower-to-sunnyhurst-brook');
 });
 
-test('extract url from rss update message', () => {
-    expect(xmlBodyJson.body).toBe('empty body');
+test('extract url from rss update message', async () => {
+    const result = await index.handler(xmlBodyJson, null);
+    expect(result).toBe('empty body');
 });
